fd8430a1972ef91f758cdf5c0076ca1e4127c2b2
TricellQwQ <118746648+TricellQwQ@users.noreply.github.com>
2023-09-19 15:32:06 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_wtf.csrf import CSRFProtect
 from forms.application import ApplicationFrom
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "P++G!+=]cP8tuJ-T:a9sYZaDaD=H))"
+
 
 @app.route("/", methods=["GET"])
 def main():
-    return "aboba"
+    return render_template("base.html")
+
 
 @app.route("/application", methods=["GET", "POST"])
 def application():
     form = ApplicationFrom()
+    return render_template("application.html", form=form)
 
 
 if __name__ == '__main__':
+    csrf = CSRFProtect(app)
     app.run(port=8080, host='127.0.0.1', debug=True)
