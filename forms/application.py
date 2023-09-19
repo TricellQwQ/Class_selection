d6b3beccf0e0b6f687143f3a54b978c18f3e35d4
TricellQwQ <118746648+TricellQwQ@users.noreply.github.com>
2023-09-19 15:34:23 +0300

    Add files via upload

--- forms/application.py
+++ forms/application.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, RadioField
+from wtforms import StringField, RadioField, SubmitField
 from wtforms.validators import DataRequired
 
 
@@ -7,3 +7,4 @@ class ApplicationFrom(FlaskForm):
     name = StringField("Имя", validators=[DataRequired()])
     surname = StringField("Фамилия", validators=[DataRequired()])
     exams = RadioField("Экзамены", choices=["Русский язык", "Математика", "Физика", "Химия", "Биология", "Литература", "География", "История", "Информатика", "Обществознание", "Английский язык", "Французский язык", "Немецкий язык", "Испанский язык"])
+    submit = SubmitField("Отправить")
